8f68affe7db1b7e8df3283a1c6032fe2ca7a984b
HitsujiRere <55770125+HitsujiRere@users.noreply.github.com>
2025-06-18 16:59:21 +0900

    fix: `Header`コンポーネントのz-indexを追加

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "~/components/ThemeToggle";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 flex items-baseline justify-between border-b border-b-border bg-background px-4 py-2">
+    <header className="sticky top-0 z-1 flex items-baseline justify-between border-b border-b-border bg-background px-4 py-2">
       <Link href="/" className="flex gap-1">
         <span className="text-xl">🐏</span>
         <p className="font-bold text-lg">Hitzuji blog</p>
