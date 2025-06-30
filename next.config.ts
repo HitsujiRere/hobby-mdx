2f856c29634c0ada1f65104b1a381517b2e92a0c
HitsujiRere <55770125+HitsujiRere@users.noreply.github.com>
2025-06-30 11:44:41 +0900

    feat: `next.config.ts`にて`use cache`を有効化

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["next-mdx-remote"],
+  experimental: {
+    useCache: true,
+  },
 };
 
 export default nextConfig;
